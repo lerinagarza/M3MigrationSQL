5572dc16709f62ef1357a1dfdfe2f9449da1f746
aesnar <ana.hernandez-esnard@treetop.com>
2019-11-18 11:07:27 -0800

    Adding MITBAL and changing item to only status 20

--- views/MGVMITMAS.sql
+++ views/MGVMITMAS.sql
@@ -5,10 +5,12 @@
 
 --Modified
 --     Date        By                Modification Description
-
+--    11/18/19    AESNAR             Eliminate test items itty 500 and 999
+--
 --GRANT SELECT ON MIGRATION.MGVMITMAS TO M3DBREADS
 --CREATE OR REPLACE VIEW MIGRATION.MGVMITMAS AS
 
 SELECT * FROM M3FDBPRD.MITMAS
 WHERE MMCONO = 100 AND
-      MMSTAT = '20'
+      MMSTAT = '20' AND
+      MMITTY NOT IN ('500', '999')
